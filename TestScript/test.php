e4d44541e3c0b70ba1f0cc38d78362284c4b6cf2
Monica <monica.manghnani@dojo.tech>
2024-08-30 16:49:02 +0100

    Fixed money value and added autoexpireaction

--- TestScript/test.php
+++ TestScript/test.php
@@ -1,4 +1,8 @@
 <?php
+// The sandbox API key used in this example is public.
+// Don't submit any personally identifiable information in any requests made with this key.
+// Sign in to developer.dojo.tech to create your own private sandbox key and use that instead
+// for secure testing.
 
 namespace Test;
 require_once "vendor/autoload.php";
@@ -6,18 +10,24 @@ require_once "vendor/autoload.php";
 use Dojo_PHP\ApiFactory;
 use Dojo_PHP\Model\CreatePaymentIntentRequest;
 use Dojo_PHP\Model\Money;
+use Dojo_PHP\Model\CaptureMode;
+use Dojo_PHP\Model\AutoExpireAction;
 
 $apiKey = "sk_sandbox_c8oLGaI__msxsXbpBDpdtwJEz_eIhfQoKHmedqgZPCdBx59zpKZLSk8OPLT0cZolbeuYJSBvzDVVsYvtpo5RkQ";
 $apiPaymentIntent = ApiFactory::createPaymentIntentApi($apiKey);
 
 $req = new CreatePaymentIntentRequest();
-$req->setReference("test");
+$req->setDescription("Demo payment intent");
+$req->setReference("Order - 234");
 
 $money = new Money();
-$money->setValue(100);
+$money->setValue(1000);
 $money->setCurrencyCode("GBP");
 
 $req->setAmount($money);
 
+$req->setCaptureMode(CaptureMode::MANUAL);
+$req->setAutoExpireAction(AutoExpireAction::RELEASE);
+$req->setAutoExpireIn("00:00:59");
+
 $pi = $apiPaymentIntent->paymentIntentsCreatePaymentIntent(\Dojo_PHP\API_VERSION, $req);
-echo $pi;
